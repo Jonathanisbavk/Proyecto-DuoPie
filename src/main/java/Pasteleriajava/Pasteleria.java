8aa293a507c186d0383ce62b56626e6a79ae7a67
Jonathanisbavk <jonathan9579285@gmail.com>
2023-12-04 06:36:15 -0500

    Herencia, abstracción y polformismo

--- src/main/java/Pasteleriajava/Pasteleria.java
+++ src/main/java/Pasteleriajava/Pasteleria.java
@@ -2,6 +2,9 @@ package Pasteleriajava;
 
 import java.util.Scanner;
 
+
+import java.util.Scanner;
+
 public class Pasteleria {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,30 +13,30 @@ public class Pasteleria {
         String nombre = input.nextLine();
         System.out.println("Ingrese su apellido");
         String apellido = input.nextLine();
-        
-        Cliente cliente = new Cliente(nombre,apellido);
 
-        System.out.println("Hola, " + nombre + " "+ apellido  + ", elija su pastel:");
-        
-        
+        Cliente cliente = new Cliente(nombre, apellido);
+
+        System.out.println("Hola, " + nombre + " " + apellido + ", elija su pastel:");
+
         System.out.println("1.- Pasteles de Exhibición");
         System.out.println("2.- Pasteles Temáticos");
 
         String opcionPastel = input.nextLine();
 
         Pedido pedido = new Pedido();
-        
-        
+
+        Producto pastelElegido = null;
+
         if (opcionPastel.equals("1")) {
-            PastelesDeExhibicion pastelesDeExhibicion = new PastelesDeExhibicion();
-            pastelesDeExhibicion.seleccionarPastel(input, pedido);
+            pastelElegido = new PastelesDeExhibicion();
         } else if (opcionPastel.equals("2")) {
-            // Estamos trabajando acá :D
-            PastelesTematicos pastelesTematicos = new PastelesTematicos();
-            pastelesTematicos.seleccionarPastel(input,cliente, pedido);
+            pastelElegido = new PastelesTematicos();
         } else {
             System.out.println("Opción no válida.");
+            return;
         }
+
+        pastelElegido.seleccionarPastel(input, cliente, pedido);
 
         MetodoDePago metodoDePago = new MetodoDePago();
         metodoDePago.seleccionarMetodoDePago(input, pedido);
@@ -45,6 +48,7 @@ public class Pasteleria {
         System.out.println(pedido.toString());
     }
 }
+
 class Cliente{
     public String nombre;
     private String apellido;
@@ -121,36 +125,50 @@ class Pedido {
         return "\n" + resumenPedido + "Precio Total: S/" + precioTotal + "\n";
     }
 }
-class PastelesTematicos {
-    public void seleccionarPastel(Scanner input, Cliente cliente,Pedido pedido) {
-        System.out.println("¡Excelente elección! Elige uno de estos pasteles de exhibición:");
+interface Producto {
+    void seleccionarPastel(Scanner input, Cliente cliente, Pedido pedido);
+    double obtenerPrecioBase();
+}
+
+abstract class Pastel implements Producto {
+    protected double precioBase;
+
+    @Override
+    public double obtenerPrecioBase() {
+        return precioBase;
+    }
+}
+
+class PastelesTematicos extends Pastel {
+    @Override
+    public void seleccionarPastel(Scanner input, Cliente cliente, Pedido pedido) {
+        
+        System.out.println("¡Excelente elección! Elige uno de estos pasteles temáticos:");
         System.out.println("1.- Torta Infantil - Precio: S/80.00");
         System.out.println("2.- Torta Matrimonial - Precio: S/75.00");
-        System.out.println("3.- Torta Quinciañera - Precio: S/70.00");
+        System.out.println("3.- Torta Quinceañera - Precio: S/70.00");
 
         String opcionPastelex = input.nextLine();
-        double precioPastelex = 0.0;
 
         switch (opcionPastelex) {
             case "1":
-                precioPastelex = 80.0;
+                precioBase = 80.0;
                 pedido.agregarResumen("Pastel Escogido: Infantil\nCosto Base: S/80.00\n");
                 break;
             case "2":
-                precioPastelex = 75.0;
+                precioBase = 75.0;
                 pedido.agregarResumen("Pastel Escogido: Matrimonial\nCosto Base: S/75.00\n");
                 break;
             case "3":
-                precioPastelex = 70.0;
+                precioBase = 70.0;
                 pedido.agregarResumen("Pastel Escogido: Quinceañera\nCosto Base: S/70.00\n");
                 break;
             default:
                 System.out.println("Opción no válida.");
-                break;
+                return; 
         }
-        
 
-        pedido.agregarPrecio(precioPastelex);
+        pedido.agregarPrecio(precioBase);
 
         System.out.println("Elija la cantidad de porciones:");
         System.out.println("1.- 12 porciones");
@@ -173,36 +191,35 @@ class PastelesTematicos {
     }
 }
 
-
-class PastelesDeExhibicion {
-    public void seleccionarPastel(Scanner input, Pedido pedido) {
+class PastelesDeExhibicion extends Pastel {
+    @Override
+    public void seleccionarPastel(Scanner input, Cliente cliente, Pedido pedido) {
         System.out.println("¡Excelente elección! Elige uno de estos pasteles de exhibición:");
         System.out.println("1.- Tres Leches - Precio: S/80.00");
         System.out.println("2.- Torta Selva Negra - Precio: S/75.00");
         System.out.println("3.- Torta de Oreo - Precio: S/70.00");
 
         String opcionPastelex = input.nextLine();
-        double precioPastelex = 0.0;
 
         switch (opcionPastelex) {
             case "1":
-                precioPastelex = 80.0;
+                precioBase = 80.0;
                 pedido.agregarResumen("Pastel Escogido: Tres Leches\nCosto Base: S/80.00\n");
                 break;
             case "2":
-                precioPastelex = 75.0;
+                precioBase = 75.0;
                 pedido.agregarResumen("Pastel Escogido: Torta Selva Negra\nCosto Base: S/75.00\n");
                 break;
             case "3":
-                precioPastelex = 70.0;
+                precioBase = 70.0;
                 pedido.agregarResumen("Pastel Escogido: Torta de Oreo\nCosto Base: S/70.00\n");
                 break;
             default:
                 System.out.println("Opción no válida.");
-                break;
+                return; 
         }
 
-        pedido.agregarPrecio(precioPastelex);
+        pedido.agregarPrecio(precioBase);
 
         System.out.println("Elija la cantidad de porciones:");
         System.out.println("1.- 12 porciones");
